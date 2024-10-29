b92b797631f919f2e6a685fbf89141873c99ba7d
namespace-red <67328203+namespace-red@users.noreply.github.com>
2024-10-29 22:13:50 +0300

    Use IPickable in Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -50,10 +50,18 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.TryGetComponent(out Money money))
+        if (other.TryGetComponent(out IPickable pickated))
         {
-            _wallet.AddMoney(money.Value);
-            money.PickUp();
+            switch (pickated)
+            {
+                case Money money:
+                    _wallet.AddMoney(money.Value);
+                    money.PickUp();
+                    break;
+
+                default:
+                    throw new ArgumentException($"Not correct type {pickated}");
+            }
         }
     }
 }
