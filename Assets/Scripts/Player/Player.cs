421e90ef4b3bec013a1e9a5a9436f87d280edbf8
namespace-red <67328203+namespace-red@users.noreply.github.com>
2024-10-31 21:37:11 +0300

    UserInput script

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,14 +1,76 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(HorizontalPhysicsMover2D))]
+[RequireComponent(typeof(HorizontalRotater2D))]
+[RequireComponent(typeof(Jumper2D))]
+[RequireComponent(typeof(FallDetector2D))]
 [RequireComponent(typeof(Wallet))]
 public class Player : MonoBehaviour
 {
+    [SerializeField] private PlayerAnimationsController _animationsController;
+    [SerializeField] private UserInput _userInput;
+    [SerializeField] private GroundDetector2D _groundDetector;
+    [SerializeField] private bool _canControlledInAir;
+
+    private HorizontalPhysicsMover2D _mover;
+    private HorizontalRotater2D _rotater;
+    private Jumper2D _jumper;
+    private FallDetector2D _fallDetector;
     private Wallet _wallet;
+    private bool _isJumped;
 
     private void Awake()
     {
+        if (_animationsController == null) 
+            throw new NullReferenceException(nameof(_animationsController));
+        
+        if (_userInput == null) 
+            throw new NullReferenceException(nameof(_userInput));
+        
+        if (_groundDetector == null) 
+            throw new NullReferenceException(nameof(_groundDetector));
+        
+        _mover = GetComponent<HorizontalPhysicsMover2D>();
+        _rotater = GetComponent<HorizontalRotater2D>();
+        _jumper = GetComponent<Jumper2D>();
+        _fallDetector = GetComponent<FallDetector2D>();
         _wallet = GetComponent<Wallet>();
+    }
+    
+    private void OnEnable()
+    {
+        _userInput.JumpPressed += OnJumpPressed;
+        _jumper.Jumping += _animationsController.PlayJump;
+        _fallDetector.Falling += _animationsController.PlayFall;
+        _groundDetector.Grounded += _animationsController.OnGrounded;
+    }
+    
+    private void OnDisable()
+    {
+        _userInput.JumpPressed -= OnJumpPressed;
+        _jumper.Jumping -= _animationsController.PlayJump;
+        _fallDetector.Falling -= _animationsController.PlayFall;
+        _groundDetector.Grounded -= _animationsController.OnGrounded;
+    }
+
+    private void FixedUpdate()
+    {
+        _animationsController.SetRunState(_userInput.HorizontalInput != 0);
+        
+        if ((_userInput.HorizontalInput != 0f) && 
+            (_canControlledInAir || _groundDetector.IsGrounding))
+        {
+            _rotater.Rotate(_userInput.HorizontalInput);
+            _mover.Move(_userInput.HorizontalInput);
+        }
+        
+        if (_isJumped && _groundDetector.IsGrounding)
+        {
+            _jumper.Jump();
+        }
+        
+        _isJumped = false;
     }
     
     private void OnTriggerEnter2D(Collider2D other)
@@ -26,5 +88,10 @@ public class Player : MonoBehaviour
                     throw new ArgumentException($"Not correct type {pickated}");
             }
         }
+    }
+
+    private void OnJumpPressed()
+    {
+        _isJumped = true;
     }
 }
---
+++ Assets/Scripts/Player/UserInput.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    private const string Horizontal = nameof(Horizontal);
+    private const string Jump = nameof(Jump);
+
+    public float HorizontalInput { get; private set; }
+    
+    public event Action JumpPressed;
+    
+    private void Update()
+    {
+        HorizontalInput = Input.GetAxis(Horizontal);
+        
+        if (Input.GetButtonDown(Jump))
+            JumpPressed?.Invoke();
+    }
+}
