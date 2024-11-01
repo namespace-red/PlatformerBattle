1c076ddb14ed7b2db9f678c072f4f35a989cd017
namespace-red <67328203+namespace-red@users.noreply.github.com>
2024-11-01 11:49:40 +0300

    Counter in GroundDetector2D

--- Assets/Scripts/Movement/Components/GroundDetector2D.cs
+++ Assets/Scripts/Movement/Components/GroundDetector2D.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class GroundDetector2D : MonoBehaviour
 {
+    [SerializeField]private int _count;
     public event Action Grounded;
     
     public bool IsGrounding { get; private set; }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.isTrigger)
+            return;
+        
         IsGrounding = true;
-        Grounded?.Invoke();
+
+        if (++_count == 1)
+            Grounded?.Invoke();
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        IsGrounding = false;
+        if (other.isTrigger)
+            return;
+
+        if (--_count == 0)
+            IsGrounding = false;
+        
+        else if (_count < 0)
+            throw new IndexOutOfRangeException(nameof(_count));
     }
 }
