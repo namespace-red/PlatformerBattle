01546094ef2b2182688c73469dd5c2c2639c70da
namespace-red <67328203+namespace-red@users.noreply.github.com>
2024-11-10 22:30:22 +0300

    Сleaned up the trash

--- Assets/Scripts/Character/Enemies/Pusher.cs
+++ Assets/Scripts/Character/Enemies/Pusher.cs
@@ -8,7 +8,6 @@ public class Pusher : MonoBehaviour
     public void Push(Rigidbody2D rigidbody2D, Vector2 direction)
     {
         Vector2 velocity = new Vector2(direction.x * _force * _horizontalMultiplier, direction.y * _force);
-        Debug.Log(velocity);
         rigidbody2D.velocity = Vector2.zero;
         rigidbody2D.AddForce(velocity, ForceMode2D.Force);
     }
